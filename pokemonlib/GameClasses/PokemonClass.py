6412b354592546f91572bbfdb9d7907fec0743d4
MainComputer <papinrouge@gmail.com>
2019-08-20 10:35:24 +0200

    fixed __init__() in PokemonClass
    The "fix" before wasn't tested and I had to fix many errors but now it should work just fine but on windows only.

--- pokemonlib/GameClasses/PokemonClass.py
+++ pokemonlib/GameClasses/PokemonClass.py
@@ -8,23 +8,23 @@ class Pokemon:
     def __init__(self, pokemon_id=1):
         self.id = pokemon_id
         pokemon = []
-        localPath = os.listdir(os.getcwd().split("/"))
+        localPath = os.getcwd().split("\\")
         print(localPath)
         path = ""
-        for x in range(len(localPath)-1):
-            print(x)
-            path += path + str(localPath[x])
-        for filename in os.listdir(path):
+        for x in range(len(localPath)):
+            path += str(localPath[x]) + "\\"
+        path += "pokemonlib\\pokemon_data\\pokemon_properties\\"
+        for filename in os.listdir(path + "\\"):
             if filename != "pokemon_" + str(self.id) + ".json":
                 continue
             else:
-                f = open(filename, "r")
+                f = open(path+filename, "r")
                 pokemon = json.load(f)
                 print("Pokemon ´" + pokemon["name"] + "´ initializing.")
                 f.close()
                 break
         if pokemon == []:
-            raise Exeptions.IdNotReferenced("The pokemon_data with id " + str(self.id) + " was not found in the directory ´pokemon_data´. Make sure it is in a file named pokemon_(id of the pokemon_data).json")
+            raise Exeptions.IdNotReferenced("The pokemon_data with id " + str(self.id) + " was not found in the directory ´"+path+"´. Make sure it is in a file named pokemon_(id of the pokemon_data).json")
 
         stats = pokemon["stats"]
         self.name = pokemon["name"]
@@ -34,7 +34,7 @@ class Pokemon:
         self.attkph = stats["attack"]
         self.attksp = stats["special-defense"]
         self.lv = 1
-        self.types = pokemon["type"]
+        self.types = pokemon["types"]
         self.attacks = {1: ["Thunderbolt", 20, True], 2: ["Poop", 40, True], 3: ["Earthquake", 40, True], 4: ["Scratch", 40, True]}
         self.iv = random.randint(0, 31)
 
