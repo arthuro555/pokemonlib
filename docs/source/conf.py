86f760016902f248fa08aed6893a0681d45e2535
MainComputer <papinrouge@gmail.com>
2019-09-08 09:40:59 +0200

    try again sphinx doc?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,8 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'Pokemonlib'
-copyright = '2019, Arthur Pacuad (arthuro555)'
-author = 'Arthur Pacuad (arthuro555)'
+copyright = '2019, Arthur Pacaud (arthuro555)'
+author = 'Arthur Pacaud (arthuro555)'
 
 
 # -- General configuration ---------------------------------------------------
@@ -36,7 +36,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,5 +1,5 @@
 .. Pokemonlib documentation master file, created by
-   sphinx-quickstart on Sun Sep  8 09:20:48 2019.
+   sphinx-quickstart on Sun Sep  8 09:37:37 2019.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
