baede5cfc37cfb13d05bf44270b5af0e006eaa99
MainComputer <papinrouge@gmail.com>
2019-09-08 09:55:37 +0200

    PLS WORK SPHINX

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.append("/home/docs/checkouts/readthedocs.org/user_builds/pokemonlib/checkouts/latest/")
 
 
 # -- Project information -----------------------------------------------------
