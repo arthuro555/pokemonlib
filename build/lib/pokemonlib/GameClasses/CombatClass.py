10583a2f7f9f91135342a202923d2f18a73e7556
Arthur Pacaud <arthur.pacaud@hotmail.fr>
2020-08-23 10:11:34 +0000

    Remove junk

--- build/lib/pokemonlib/GameClasses/CombatClass.py
+++
@@ -1,8 +0,0 @@
-class Combat:
-    def __init__(self, team_one, team_two):
-        self.team1 = team_one
-        self.team2 = team_two
-
-    def damage_calculation(self, pokemon_attacking, pokemon_defending):
-        pass
-
