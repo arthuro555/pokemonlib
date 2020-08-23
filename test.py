1299eae9aede36f7e6df1842ce597b1a3250e3f1
arthuro555 <arthur.pacaud@hotmail.fr>
2020-08-23 15:53:34 +0200

    Refactor test.py

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ class TestPokemon(unittest.TestCase):
         attacks = {1: ["Thunderbolt", 20, True], 2: ["Poop", 40, True], 3: ["Earthquake", 40, True],
                    4: ["Scratch", 40, True]}
         self.assertEqual(pokemon.get_stats(), {"speed": 90, "defsp": 50, "attksp": 50, "def": 40, "attk": 55,
-                                              "hp": 35, "attacks": attacks})
+                                               "hp": 35, "attacks": attacks})
 
 
 class TestTeam(unittest.TestCase):
@@ -56,26 +56,20 @@ class TestBox(unittest.TestCase):
         self.assertEqual(box._Box__pokemonlist[0]._Pokemon__id, identifier)
         self.assertEqual(box._Box__pokemonlist[1]._Pokemon__id, identifier2)
 
-
-class PokemonClassDotPyTests(unittest.TestCase):
     def test_box_team_transfer(self):
-        pika = Pokemon(randint(1, 800))
-        chu = Pokemon(randint(1, 800))
-        ka = Pokemon(randint(1, 800))
-        team = Team(pika)
-        boxie = Box()
-        Boxar = Box(chu)
-        Boxar.transfer(team, 0)
-        boxie.add(ka)
-        team.transfer(boxie, 1)
-        team.transfer(Boxar, 0)
-        self.assertEqual(Boxar._Box__pokemonlist[0], pika)
-        self.assertEqual(boxie._Box__pokemonlist[0], ka)
-        self.assertEqual(boxie._Box__pokemonlist[1], chu)
-
-
-
-
+        pokemon1 = Pokemon(randint(1, 800))
+        pokemon2 = Pokemon(randint(1, 800))
+        pokemon3 = Pokemon(randint(1, 800))
+        team = Team(pokemon1)
+        box1 = Box()
+        box2 = Box(pokemon2)
+        box2.transfer(team, 0)
+        box1.add(pokemon3)
+        team.transfer(box1, 1)
+        team.transfer(box2, 0)
+        self.assertEqual(box2._Box__pokemonlist[0], pokemon1)
+        self.assertEqual(box1._Box__pokemonlist[0], pokemon3)
+        self.assertEqual(box1._Box__pokemonlist[1], pokemon2)
 
 
 if __name__ == '__main__':
